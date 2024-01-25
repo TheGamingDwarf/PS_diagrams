2412faab253a02e70108a1c86961492fb3dae792
Mike Kamerman <nlmike80@hotmail.nl>
2024-01-25 14:26:09 +0100

    gecomprimeerde robot bestand toegevoegd

--- Robot_program/Process_and_Safety_V2/Process_and_Safety_V2/Virtual Controllers/CRB1100_4_47/internal/opt_l0.cmd
+++
@@ -1,6 +0,0 @@
-
-include -path "C:/Users/mike kamerman/AppData/Local/ABB/RobotWare/RobotControl_7.10.0/options/pnet/pn_opt.cmd"
-
-include -path "C:/Users/mike kamerman/AppData/Local/ABB/RobotWare/RobotControl_7.10.0/options/dnmss/Dnet_opt.cmd"
-
-include -path "C:/Users/mike kamerman/AppData/Local/ABB/RobotWare/RobotControl_7.10.0/options/sc/ScInc_opt.cmd"
--- Robot_program/Process_and_Safety_V2/Process_and_Safety_V2/Virtual Controllers/CRB1100_4_47/internal/option.cmd
+++
@@ -1,2 +0,0 @@
-
-include -path "C:/Users/mike kamerman/AppData/Local/ABB/RobotWare/RobotControl_7.10.0/options/sc/scopt.cmd"
